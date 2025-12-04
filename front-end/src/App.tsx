7d48e379faa6fe4e1791dc20bc7e0c43e997b74a
yang1420 <songyouyang1420@gmail.com>
2025-12-04 02:52:03 -0800

    remove comment

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,33 +3,7 @@ import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./App.css";
-//const ENABLE_GSI = import.meta.env.VITE_ENABLE_GSI === "true";
 
-// function App() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   const handleLogin = () => {
-//     setIsLoggedIn(true);
-//   };
-
-//   const handleLogout = () => {
-//     setIsLoggedIn(false);
-//   };
-
-//   {ENABLE_GSI && (
-//     return (
-//     // ⚠️ 这里替换成你自己的 Client ID
-//     <GoogleOAuthProvider clientId="219694033881-4od4hi84uakag1cf7fuucm6s6u8q7ef9.apps.googleusercontent.com">
-//       {isLoggedIn ? (
-//         <HomePage onLogout={handleLogout} />
-//       ) : (
-//         <LoginPage onLogin={handleLogin} />
-//       )}
-//     </GoogleOAuthProvider>
-//   );
-//   )}
-//   return true;
-// }
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const ENABLE_GSI = import.meta.env.VITE_ENABLE_GSI === "true";
@@ -47,13 +21,7 @@ function App() {
         <LoginPage onLogin={handleLogin} />
       )}
     </div>
-    /*(
-      {isLoggedIn ? (
-        <HomePage onLogout={handleLogout} />
-      ) : (
-        <LoginPage onLogin={handleLogin} />
-      )}
-    );*/
+
   }
 
   // 启用 Google
