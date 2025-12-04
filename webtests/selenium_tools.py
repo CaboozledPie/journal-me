104c9368d5e1de4f3d1a9ec96fd09e3093327ed2
CaboozledPie <andrewprasetya05@gmail.com>
2025-12-04 04:25:46 -0800

    got a webtest working and made some small server-side changes

--- webtests/selenium_tools.py
+++ webtests/selenium_tools.py
@@ -15,15 +15,17 @@ def wait_class(driver, class_name):
     return element
 
 def wait_id_text(driver, id, text):
-    wait_id(driver, id)
-    element = WebDriverWait(driver, 10).until(
-        EC.text_to_be_present_in_element((By.ID, id), text)
+    # waits for an element of a given id that contains the given text, times out after 10s
+    xpath = f"//*[contains(@id, '{id}') and contains(text(), '{text}')]"
+    element = WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located((By.XPATH, xpath))
     )
     return element
 
 def wait_class_text(driver, class_name, text):
-    wait_class(driver, class_name)
+    # waits for an element of a given class that contains the given text, times out after 10s
+    xpath = f"//*[contains(@class, '{class_name}') and contains(text(), '{text}')]"
     element = WebDriverWait(driver, 10).until(
-        EC.text_to_be_present_in_element((By.CLASS_NAME, class_name), text)
+        EC.presence_of_element_located((By.XPATH, xpath))
     )
     return element
--- webtests/test_basic.py
+++ webtests/test_basic.py
@@ -7,10 +7,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import selenium_tools
 
+from uuid import uuid4
 import time
 
 
 def test_basic():
+    # unique id to avoid clashing tests
+    test_id = str(uuid4())
+
     # Start Chrome
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -24,47 +28,50 @@ def test_basic():
 
         # add two new posts 
         selenium_tools.wait_id(driver, "title")
-        title_input = driver.find_element(By.ID, "title").send_keys("Test 1")
-        content_input = driver.find_element(By.ID, "content").send_keys("i love cs35l!")
+        title_input = driver.find_element(By.ID, "title").send_keys("Test 1 " + test_id)
+        content_input = driver.find_element(By.ID, "content").send_keys("i love cs35l! " + test_id)
         submit_button = driver.find_element(By.CLASS_NAME, "add-post-btn").click()
 
-        assert selenium_tools.wait_class_text(driver, "post-title", "Test 1")
-        assert selenium_tools.wait_class_text(driver, "post-content", "i love cs35l!")
+        assert selenium_tools.wait_class_text(driver, "post-title", "Test 1 " + test_id)
+        assert selenium_tools.wait_class_text(driver, "post-content", "i love cs35l! " + test_id)
 
-        title_input = driver.find_element(By.ID, "title").send_keys("Test 2")
-        content_input = driver.find_element(By.ID, "content").send_keys("i hate cs35l!")
+        time.sleep(1) # forms can get overloaded
+
+        title_input = driver.find_element(By.ID, "title").send_keys("Test 2 " + test_id)
+        content_input = driver.find_element(By.ID, "content").send_keys("i hate cs35l! " + test_id)
         submit_button = driver.find_element(By.CLASS_NAME, "add-post-btn").click()
 
-        assert selenium_tools.wait_class_text(driver, "post-title", "Test 2")
-        assert selenium_tools.wait_class_text(driver, "post-content", "i hate cs35l!")
+        assert selenium_tools.wait_class_text(driver, "post-title", "Test 2 " + test_id)
+        assert selenium_tools.wait_class_text(driver, "post-content", "i hate cs35l! " + test_id)
 
         # assert items are still there
         driver.refresh()
         selenium_tools.wait_id(driver, "skip-google-btn")
         login_button = driver.find_element(By.ID, "skip-google-btn").click()
         
-        breakpoint()
-        assert selenium_tools.wait_class_text(driver, "post-title", "Test 1")
-        assert selenium_tools.wait_class_text(driver, "post-content", "i love cs35l!")
+        assert selenium_tools.wait_class_text(driver, "post-title", "Test 1 " + test_id)
+        assert selenium_tools.wait_class_text(driver, "post-content", "i love cs35l! " + test_id)
 
-        assert selenium_tools.wait_class_text(driver, "post-title", "Test 2")
-        assert selenium_tools.wait_class_text(driver, "post-content", "i hate cs35l!")
+        assert selenium_tools.wait_class_text(driver, "post-title", "Test 2 " + test_id)
+        assert selenium_tools.wait_class_text(driver, "post-content", "i hate cs35l! " + test_id)
 
         # test delete
-        delete_button1 = driver.find_element(By.XPATH, "//button[@class='delete-post-btn' and @id='block-0']")
-        delete_button2 = driver.find_element(By.XPATH, "//button[@class='delete-post-btn' and @id='block-0']")
+        delete_button1 = driver.find_element(By.XPATH, "//button[@class='delete-post-btn' and @id='delete-0']")
+        delete_button2 = driver.find_element(By.XPATH, "//button[@class='delete-post-btn' and @id='delete-1']")
         delete_button1.click()
         delete_button2.click()
 
         # assert removed from ui
-        assert driver.find_element(By.ID, 'post-title-0') == False
-        assert driver.find_element(By.ID, 'post-title-1') == False
+        assert len(driver.find_elements(By.XPATH, f"//h3[@id='post-content-0' and text()='i hate cs35l! {test_id}']")) == 0
+        assert len(driver.find_elements(By.XPATH, f"//h3[@id='post-content-1' and text()='i love cs35l! {test_id}']")) == 0
 
         # refresh and assert permanent removal
         driver.refresh()
+        selenium_tools.wait_id(driver, "skip-google-btn")
+        login_button = driver.find_element(By.ID, "skip-google-btn").click()
         selenium_tools.wait_id(driver, "title")
-        assert driver.find_element(By.XPATH, "//h3[@id='post-content-0' and text()='i love cs35l!']") == False
-        assert driver.find_element(By.XPATH, "//h3[@id='post-content-1' and text()='i love cs35l!']") == False
+        assert len(driver.find_elements(By.XPATH, f"//h3[@id='post-content-0' and text()='i hate cs35l! {test_id}']")) == 0
+        assert len(driver.find_elements(By.XPATH, f"//h3[@id='post-content-1' and text()='i love cs35l! {test_id}']")) == 0
 
         # logout
         selenium_tools.wait_class(driver, "logout-btn").click()
