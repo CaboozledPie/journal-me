ebcf10fedce6c5ed8d0dcb81b1d341a9608affbe
yang1420 <songyouyang1420@gmail.com>
2025-12-04 02:50:39 -0800

    fix the problem of build modle

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,20 +3,61 @@ import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./App.css";
+//const ENABLE_GSI = import.meta.env.VITE_ENABLE_GSI === "true";
 
+// function App() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   const handleLogin = () => {
+//     setIsLoggedIn(true);
+//   };
+
+//   const handleLogout = () => {
+//     setIsLoggedIn(false);
+//   };
+
+//   {ENABLE_GSI && (
+//     return (
+//     // ⚠️ 这里替换成你自己的 Client ID
+//     <GoogleOAuthProvider clientId="219694033881-4od4hi84uakag1cf7fuucm6s6u8q7ef9.apps.googleusercontent.com">
+//       {isLoggedIn ? (
+//         <HomePage onLogout={handleLogout} />
+//       ) : (
+//         <LoginPage onLogin={handleLogin} />
+//       )}
+//     </GoogleOAuthProvider>
+//   );
+//   )}
+//   return true;
+// }
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const ENABLE_GSI = import.meta.env.VITE_ENABLE_GSI === "true";
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
+  const handleLogin = () => setIsLoggedIn(true);
+  const handleLogout = () => setIsLoggedIn(false);
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
+  if (!ENABLE_GSI) {
+    // 不启用 Google，直接展示 skip 版 LoginPage
+  
+    return <div>
+      {isLoggedIn ? (
+        <HomePage onLogout={handleLogout} />
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
+    </div>
+    /*(
+      {isLoggedIn ? (
+        <HomePage onLogout={handleLogout} />
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
+    );*/
+  }
 
+  // 启用 Google
   return (
-    // ⚠️ 这里替换成你自己的 Client ID
     <GoogleOAuthProvider clientId="219694033881-4od4hi84uakag1cf7fuucm6s6u8q7ef9.apps.googleusercontent.com">
       {isLoggedIn ? (
         <HomePage onLogout={handleLogout} />
@@ -26,5 +67,6 @@ function App() {
     </GoogleOAuthProvider>
   );
 }
+
 
 export default App;
